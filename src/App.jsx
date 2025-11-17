e20ee924dc999baeededd02b13a3df5392f23a8a
Jay <jayd1903@gmail.com>
2025-11-16 22:34:45 -0800

    let's start here

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: "rgb(16, 15, 15)", display: "flex", justifyContent: "center", alignItems: "flex-start", paddingTop: "20px" }}>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <h1>stay hungry, stay foolish</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
