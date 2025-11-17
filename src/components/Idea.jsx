670c2fe92a2a4e5c7fd43c01be76c1115f3c70fc
Jay <jayd1903@gmail.com>
2025-11-17 14:43:09 -0800

    showing idea

---
+++ src/components/Idea.jsx
@@ -0,0 +1,15 @@
+import './idea.css';
+
+const Idea = () => {
+    return (
+        <div>
+            <h2>Title 1</h2>
+            <p>This is the first body text.</p>
+            <h2>Title 2</h2>
+            <p>This is the second body text.</p>
+            <p>This is the third body text.</p>
+        </div>
+    );
+};
+
+export default Idea;
